69fb5e368b2a529a1d2bc25d490bd8abc182b242
LahiruNE <lahiruepa@gmail.com>
2017-09-11 00:42:18 +0530

    Commit 1

--- protected/config/database.php
+++ protected/config/database.php
@@ -8,7 +8,7 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=vmit',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => 'root',
+	'password' => '',
 	'charset' => 'utf8',
 	
 );
